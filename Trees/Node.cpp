4e45fbc84cd2303200093c66c0625c56d0b3e9ec
ompatil-711 <ompatilll.001@gmail.com>
2025-08-05 14:16:29 +0530

    Tree

---
+++ Trees/Node.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* right;
+        Node* left;
+        Node(int){
+            this->val=val;
+            this->right=NULL;
+            this->left=NULL;
+        }
+};
+int main(){
+    Node* a= new Node(1);
+    Node* b= new Node(2);
+    Node* c= new Node(3);
+    Node* d= new Node(4);
+    Node* e= new Node(5);
+    Node* f= new Node(6);
+    Node* g= new Node(7);
+
+
+}
