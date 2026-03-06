dc3e6f55b89ec8c78a5944ed3b57bf71df2e7fd7
ompatil-711 <ompatilll.001@gmail.com>
2026-03-06 12:06:52 +0530

    Duplicate

---
+++ Practice/07.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int> arr;
+
+    for(int i=0;i<arr.size();i++){
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+}
