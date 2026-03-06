045ea6d2ca59f249364ef4dd6c0d68bfd3094db1
ompatil-711 <ompatilll.001@gmail.com>
2026-03-06 12:06:22 +0530

    palindrome

---
+++ Practice/04.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    int i = 0;
+    int j = s.size() - 1;
+
+    while(i <= j){
+        if(s[i] != s[j]){
+            cout << "false";
+            return 0;
+        }
+        i++;
+        j--;
+    }
+
+    cout << "true";
+}
